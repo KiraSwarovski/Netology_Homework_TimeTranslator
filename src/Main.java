dafe0ef29f23598e2dae2a4f779025ee68eebf0b
KiraSwarovski <jhosh1212@gmail.com>
2021-10-05 17:34:40 +0700

    4th commit

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String year = scanner.nextLine();
+        String year_nonValid = scanner.nextLine();
+        int year = Integer.parseInt(year_nonValid);
+        if (year%400==0 || year%4==0 && year%100!=0) System.out.println("Количество дней 366");
+        else System.out.println("Количество дней 365");
     }
 }
