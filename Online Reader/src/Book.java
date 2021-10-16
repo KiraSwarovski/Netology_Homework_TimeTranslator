e529ecc04bbd5c18dc75eec97d7c1dc0b2838367
KiraSwarovski <jhosh1212@gmail.com>
2021-10-16 18:03:14 +1000

    12th commit

--- Online Reader/src/Book.java
+++ Online Reader/src/Book.java
@@ -2,22 +2,17 @@ public class Book {
     private String bookName;
     private int pageNumber;
     private int priceOfBook;
+    private static int memorySize = 0;
     private SheetSize sheetSize;
     private Publisher publisher;
 
-    public Book(String bookName, int pageNumber, int priceOfBook, SheetSize sheetSize, Publisher publisher) {
+    public Book(String bookName, int pageNumber, int priceOfBook, int memorySize, SheetSize sheetSize, Publisher publisher) {
         this.bookName = bookName;
         this.pageNumber = pageNumber;
         this.priceOfBook = priceOfBook;
+        this.memorySize++;
         this.sheetSize = sheetSize;
         this.publisher = publisher;
-    }
-
-    @Override
-    public String toString() {
-        return ("Book:" + bookName + "\n Number of Page: " + pageNumber + "\n+ The price: "
-                + priceOfBook + "\n Paper size: " + sheetSize
-                + "\n Publisher: " + publisher);
     }
 
     public int DiffPrice(Book book) {
@@ -30,11 +25,17 @@ public void  CompareSheetNumber(Book book){
         else if (compareSheetNumber>0)System.out.println(this.bookName+" has more page than "+book.bookName);
         else System.out.println(this.bookName+" has equal page to "+book.bookName);
 }
-public void ListsOfPagesForPrinting(Book book){
+public void ListsOfPagesForScanning(Book book){
        int bookCover = 2;
-        if(this.pageNumber%2==0) System.out.println(this.bookName+" needs "+this.pageNumber/2+2+" lists of page for printing");
-        else System.out.println(this.bookName+" needs "+this.pageNumber/2+2+" lists of page for printing");
+        if(this.pageNumber%2==0) System.out.println(this.bookName+" needs "+this.pageNumber/2+bookCover+" lists of page for scanning");
+        else System.out.println(this.bookName+" needs "+this.pageNumber/2+bookCover+1+" lists of page for scanning");
 }
+@Override
+    public String toString() {
+        return ("Book:" + bookName + "\n Number of Page: " + pageNumber + "\n+ The price: "
+                + priceOfBook + "\n Paper size: " + sheetSize
+                + "\n Publisher: " + publisher);
+    }
     }
 
 
