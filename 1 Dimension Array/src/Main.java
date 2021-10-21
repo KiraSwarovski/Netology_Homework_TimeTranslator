308a5a551030edf311dba114d70e3153bd705dec
KiraSwarovski <jhosh1212@gmail.com>
2021-10-21 22:24:46 +1000

    15th commit

--- 1 Dimension Array/src/Main.java
+++ 1 Dimension Array/src/Main.java
@@ -3,25 +3,29 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        String[] products = arrayToPrintProducts();
-        int[] prices = arrayToPrintPricesOfProduct();
-        int count = 0;
-        for (int i = 0; i < products.length; i++) {
-                count++;
-                System.out.println(count + ". " + products[i] +" "+ prices[i]+" dollar apiece" + "\t");
-            }
-
+        String[] products = arrayToAddProducts();
+        int[] prices = arrayToAddPricesOfProduct();
+        System.out.println();
+        arrayToPrint(products, prices);
 
     }
 
-    public static String[] arrayToPrintProducts() {
+    public static String[] arrayToAddProducts() {
         String[] arrayOfProducts = {"Milk", "Bread", "Cigarettes"};
         return arrayOfProducts;
     }
 
-    public static int[] arrayToPrintPricesOfProduct() {
+    public static int[] arrayToAddPricesOfProduct() {
         int[] prices = {70, 50, 150};
         return prices;
     }
 
+    public static void arrayToPrint(String[] prod, int[] price) {
+        int count = 0;
+        for (int i = 0; i < prod.length; i++) {
+            count++;
+            System.out.println(count + ". " + prod[i] + " " + price[i] + " dollar apiece" + "\t");
+        }
+
+    }
 }
