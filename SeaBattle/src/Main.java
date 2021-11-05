5ce0576d7293be3a5ed215c3f85139a0d3edcd79
Admin <jhosh1212@gmail.com>
2021-11-05 18:13:34 +0700

    32 commit from PC

--- SeaBattle/src/Main.java
+++ SeaBattle/src/Main.java
@@ -6,12 +6,13 @@ public class Main {
     public static final int SHIP = 1;
     public static final int DEAD = 2;
     public static final int MISS = 3;
-    public static final int MAX_SHIP = 10;
-    public static final int MAX_TURN = 30;
+    public static final int MAX_SHIP = 1;
+    public static final int MAX_TURN = 2;
     public static final int SIZE = 10;
 
     public static void main(String[] args) {
         int turnCount = 0;
+        int deadCount = 0;
         int warField[][] = new int[SIZE][SIZE];
         Random random = new Random();
         for (int i = 0; i < MAX_SHIP; i++) {
@@ -22,15 +23,31 @@ public class Main {
             }
             warField[shipPlace1][shipPlace2] = SHIP;
         }
-        printArray(warField);
-        Scanner scanner = new Scanner(System.in);
-        while (turnCount <= 10) {
-            int fire = Integer.parseInt(scanner.nextLine());
 
-            switch (fire) {
-                case 1:
+        Scanner scanner = new Scanner(System.in);
+        while (turnCount<=MAX_TURN) {
+            printArray(warField);
+            int fireY = scanner.nextInt()-1;
+            int fireX = scanner.nextInt()-1;
+            switch (warField[fireY][fireX]) {
+                case EMPTY:
+                    turnCount++;
+                    warField[fireY][fireX] = MISS;
+                    break;
+                case SHIP:
+                    turnCount++;
+                    deadCount++;
+                    warField[fireY][fireX] = DEAD;
+                    break;
+                default:
+                    System.out.println("You have already fired this cell. Try again");
+                    break;
             }
+            if (deadCount == MAX_SHIP) break;
+
         }
+        scanner.close();
+        isWin(deadCount);
     }
 
     public static void printArray(int arr[][]) {
@@ -40,6 +57,19 @@ public class Main {
             }
             System.out.println();
         }
+        System.out.println("\n");
+    }
+
+    public static void isWin(int turn) {
+        switch (turn) {
+            case MAX_SHIP:
+                System.out.println("You win");
+                break;
+            default:
+                System.out.println("You lose");
+                break;
+        }
+
     }
 }
 
