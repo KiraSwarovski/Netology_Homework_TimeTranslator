98ff0c64874df6a6ce57dd319c0d0a34a9b0cb99
Admin <jhosh1212@gmail.com>
2021-11-14 03:18:22 +0700

    34 commit from PC

--- Shooter Game/src/Main.java
+++ Shooter Game/src/Main.java
@@ -1,2 +1,26 @@
+
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Player player = new Player();
+        System.out.format("У игрока %d слотов с оружием,"
+                        + " введите номер, чтобы выстрелить,"
+                        + " -1 чтобы выйти%n",
+                player.getSlotsCount()
+        );
+        int slot;
+        while (true) {
+            int input = scanner.nextInt();
+            if (input == -1) break;
+            slot = input - 1;
+            player.setOutOfRange(false);
+            player.shotWithWeapon(slot);
+            if (player.getOutOfRange() == true) {
+                System.out.println("Invalid index");
+            }
+        }
+        System.out.println("Game over!");
+    }
 }
---
+++ Shooter Game/src/Player.java
@@ -0,0 +1,37 @@
+import Weapon.*;
+
+class Player {
+    private Weapon[] weaponSlots;
+    private  boolean isOutOfRange;
+
+    public Player() {
+        weaponSlots = new Weapon[]{
+                // TODO заполнить слоты оружием
+                new BigGun(),
+                new WaterPistol()
+        };
+    }
+
+    public int getSlotsCount() {
+        return weaponSlots.length;
+    }
+
+    public void setOutOfRange(boolean isOutOfRange) {
+        this.isOutOfRange = isOutOfRange;
+    }
+
+    public boolean getOutOfRange() {
+        return isOutOfRange;
+    }
+
+    public void shotWithWeapon(int slot) {
+        //TODO обязательно сделать тернарник
+        if (slot < 0 || slot > getSlotsCount()) {
+            setOutOfRange(true);
+        } else {
+            Weapon weapon = weaponSlots[slot];
+            weapon.shot();
+        }
+
+    }
+}
