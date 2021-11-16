a3fde75a78bb1f0550d77525aed9455fc748cb7a
KiraSwarovski <jhosh1212@gmail.com>
2021-11-16 17:54:34 +0700

    27th commit from Laptop

---
+++ thisVsSuper/src/UseSuper.java
@@ -0,0 +1,29 @@
+class C {
+    public int i;
+    public void print() {
+        System.out.println("C.i = " + i);
+    }
+}
+
+class D extends C {
+    public String i;
+
+    public D(String a, int b) {
+        i = a;
+        super.i = b;
+    }
+
+    public void print() {
+        System.out.println("D.i = " + i);
+        super.print();
+    }
+}
+
+
+public class UseSuper {
+    public static void main(String[] args) {
+        D d = new D("someString", 2);
+        d.print();
+        System.out.println(d.i);
+    }
+}
