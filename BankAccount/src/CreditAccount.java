3eca76b08951b908f0bc2d9759176f8e41e757ec
Admin <jhosh1212@gmail.com>
2021-11-21 22:17:30 +0700

    40 commit from PC

--- BankAccount/src/CreditAccount.java
+++ BankAccount/src/CreditAccount.java
@@ -1,2 +1,43 @@
 public class CreditAccount extends Account {
+    public CreditAccount(int amount, String accountName) {
+        super(amount, accountName);
+    }
+
+    @Override
+    void pay(int amount) {
+        int thisTempAmount = getAmount();
+        setAmount(getAmount() - amount);
+        System.out.printf("%d $ marked off %s; %d $-> %d $ %n",
+                amount, getAccountName(), thisTempAmount, getAmount());
+    }
+
+    @Override
+    void transfer(Account account, int amount) {
+        int thisTempAmount = getAmount();
+        setAmount(getAmount() - amount);
+        System.out.printf("%d $ marked off %s; %d $-> %d $ %n",
+                amount, getAccountName(), thisTempAmount, getAmount());
+        account.addMoney(amount);
+    }
+
+    @Override
+    void addMoney(int amount) {
+        if ((getAmount() + amount) > 0) {
+            int thisTempAmount = getAmount();
+            System.out.printf("%s's can't be positives%n",
+                    getClass().getSimpleName());
+            int freeMoney = amount + getAmount();
+            setAmount(0);
+            System.out.printf("%d $ settled an %s; %d $-> %d $ %n" +
+                            "%d $ for return%n",
+                    -thisTempAmount, getAccountName(),
+                    thisTempAmount, getAmount(), freeMoney);
+        //    super.setAmount(super.getAmount()+freeMoney);
+        } else {
+            int thisTempAmount = getAmount();
+            setAmount(getAmount() + amount);
+            System.out.printf("%d $ settled an %s; %d $-> %d $ %n",
+                    amount, getAccountName(), thisTempAmount, getAmount());
+        }
+    }
 }
--- BankAccount/src/Main.java
+++ BankAccount/src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
+        SavingsAccount savingsAccount = new SavingsAccount
+                (2600, "SavingsAcnt#1");
+        CheckingAccount checkingAccount = new CheckingAccount
+                (2900, "CheckingAcnt#1");
+        CreditAccount creditAccount = new CreditAccount
+                (0, "CreditAcnt#1");
 
+        savingsAccount.transfer(checkingAccount, 100);
+        savingsAccount.addMoney(50);
+        setApart();//***********************************
+        checkingAccount.transfer(creditAccount, 1300);
+        checkingAccount.pay(100);
+        checkingAccount.addMoney(250);
+        setApart();//*************************************
+        creditAccount.transfer(checkingAccount, 200);
+        creditAccount.pay(300);
+        creditAccount.addMoney(5150);
+    }
+
+    public static void setApart() {
+        for (int i = 0; i < 50; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
