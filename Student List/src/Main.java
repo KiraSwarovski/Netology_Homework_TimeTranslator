3d88a7211b8ddc45bf89f0dd211abcc12324d06b
KiraSwarovski <jhosh1212@gmail.com>
2021-12-15 17:57:58 +0700

    50th commit from Laptop

--- Student List/src/Main.java
+++ Student List/src/Main.java
@@ -1,14 +1,38 @@
+import java.util.HashSet;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        Student students = new Student();
-        addStudent(students);
-        displayStudentsList(students);
+        HashSet<Student> studentsList = new HashSet<>();
+        addStudent(studentsList);
+        displayStudentsList(studentsList);
     }
 
-    private static void addStudent(Student student) {
+    private static void addStudent(HashSet<Student> studentsList) {
+        Scanner scanner = new Scanner(System.in);
+        String inputStudentList = "";
+        System.out.println("Enter information about the student: \"Full name, group number, student ID number\"");
+        while (!"end".equals(inputStudentList)) {
+            System.out.println("Enter information about the student (to complete the program, enter \"end\")");
+            inputStudentList = scanner.nextLine();
+            String[] studentInfo = inputStudentList.split(",");
+            Student student = null;
+            if (studentInfo.length == 3) {
+                student = new Student(studentInfo[0], studentInfo[1], studentInfo[2]);
+                if (studentsList.contains(student)) {
+                    System.out.println("Such student ID exists");
+                } else {
+                    studentsList.add(student);
+                }
+            }
 
+        }
     }
-    private static void displayStudentsList(Student student){
 
+    private static void displayStudentsList(HashSet<Student> students) {
+//        HashSet<Student> printStudent = new HashSet<>();
+        for(Student printStudent : students){
+            System.out.println(printStudent.getName()+" "+printStudent.getGroup()+" "+printStudent.getStudentId());
+        }
     }
 }
