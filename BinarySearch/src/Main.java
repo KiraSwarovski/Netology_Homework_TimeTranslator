dbec279a699235bdff5647499dc5e64d01e808c0
MaxRaitin <jhosh1212@gmail.com>
2022-02-11 10:27:51 +0700

    1

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+class BinarySearch {
+    int binary(int[] arr, int x) {
+        int last = -1;
+        for (int i = 0; i <arr.length; i++) {
+            if (x !=arr[i]){
+                continue;
+            }
+            last = i;
+        }
+        if(last!=-1) {
+            return arr.length - last - 1;
+        }
+        return -1;
+    }
 
-public class Main {
+    public static void main(String[] args) {
+        BinarySearch ob = new BinarySearch();
+        int[] arr = {14, 16, 19, 32, 32, 32, 56, 69, 72};
+        int r = arr.length;
+        int l = 0;
+        int result = ob.binary(arr, 32);
+        if (result == -1) {
+            System.out.println("Item not found");
+        } else {
+            System.out.println("Element index is " + result);
+        }
+    }
 }
--- establish/src/Main.java
+++ establish/src/Main.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        int[] numbers = {4, 7, 9, 3};
+        int output = calc(numbers);
+        System.out.println(output);
+    }
+
+    public static int calc(int[] arr) {
+        int ans = 0;
+        for (int i = 1; i < arr.length; i++) {
+            ans += arr[i] - arr[i - 1];
+        }
+        return ans;
+    }
 }
